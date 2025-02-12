e00c9a57e1565e0e421eb0c269ef514f49ee40d5
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-02-13 00:54:48 +0800

    error

--- app/Http/Controllers/IkhtisarHarianController.php
+++ app/Http/Controllers/IkhtisarHarianController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Data;
+use Carbon\Carbon;
 
 class IkhtisarHarianController extends Controller
 {
     public function index()
     {
+        // Ambil data hari ini
+        $todayData = Data::whereDate('created_at', Carbon::today())->get();
+        
+        // Ambil data terbaru
         $recentData = Data::latest()->take(5)->get();
         
-        return view('ikhtisar-harian', compact('recentData'));
+        return view('ikhtisar-harian', compact('todayData', 'recentData'));
     }
 } 
